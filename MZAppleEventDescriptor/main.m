805a519f187e0357144f74556c7731d0756cddcf
hhas <hhas@users.sourceforge.net>
2019-03-08 13:36:41 +0000

    Initial Commit

---
+++ MZAppleEventDescriptor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MZAppleEventDescriptor
+//
+//  Created by Hamish Sanderson on 08/03/2019.
+//  Copyright © 2019 Hamish Sanderson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
